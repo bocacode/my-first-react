1825c1502cf3c52bfa79449e4cafef60d8ba34a1
Todd Albert <todd@bocacode.com>
2022-05-12 16:50:21 -0400

    added some state with sauce

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [firstName, setFirstName] = useState("Todd")
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const loginLogout = () => setIsLoggedIn(!isLoggedIn)
   return (
-    <div className="App">
+    <div className="whatever-i-want">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>Welcome {isLoggedIn ? firstName : 'Guest'}.</h1>
+        <button onClick={loginLogout}>
+          {isLoggedIn ? 'Logout' : 'Login'}
+        </button>
+
+        <h2>You clicked the button {count} times.</h2>
+        <button onClick={()=>setCount(count + 1)}>+</button>&nbsp;
+        <button onClick={()=>setCount(0)}>RESET</button>&nbsp;
+        <button className='btn-red' onClick={()=>setCount(Math.max(count - 1,0))}>-</button>
+
       </header>
     </div>
   );
